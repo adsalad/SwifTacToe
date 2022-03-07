211335c6af7fcc8861ecd15ea7860849fb48eb0d
Adam S <asalaymeh@gmail.com>
2022-03-07 00:41:31 -0500

    Initial Commit

---
+++ SwifTacToe/SwifTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifTacToeApp.swift
+//  SwifTacToe
+//
+//  Created by Adam S on 2022-03-07.
+//
+
+import SwiftUI
+
+@main
+struct SwifTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
