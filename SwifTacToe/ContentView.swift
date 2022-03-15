50aec0d19d994aeb89c09b97d6db88ba2aa180b2
Adam S <asalaymeh@gmail.com>
2022-03-14 20:34:44 -0400

    Basic UI Layout

--- SwifTacToe/ContentView.swift
+++ SwifTacToe/ContentView.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
+let columns: [GridItem] = [
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, word!")
-            .padding()
+        GeometryReader { geo in
+            VStack{
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.blue).opacity(0.5)
+                                .frame(width: geo.size.width/3 - 15, height: geo.size.width/3 - 15)
+                                .padding()
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                Spacer()
+            }.padding()
+        }
     }
 }
 
