e87b00222302fcfc1dd4ad3c4ad9da4302d6efd9
Adam S <asalaymeh@gmail.com>
2022-03-14 20:56:05 -0400

    Preparing logic for computer moves

--- SwifTacToe/ContentView.swift
+++ SwifTacToe/ContentView.swift
@@ -7,13 +7,20 @@
 
 import SwiftUI
 
-let columns: [GridItem] = [
-    GridItem(.flexible()),
-    GridItem(.flexible()),
-    GridItem(.flexible()),
-]
+
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+    
     var body: some View {
         GeometryReader { geo in
             VStack{
@@ -26,16 +33,39 @@ struct ContentView: View {
                                 .frame(width: geo.size.width/3 - 15, height: geo.size.width/3 - 15)
                                 .padding()
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            if isOccupied(in: moves, forIndex: i) {
+                                return //exit case
+                            }
+                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                            isHumanTurn.toggle()
                         }
                     }
                 }
                 Spacer()
             }.padding()
         }
+    }
+    
+    func isOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: {$0?.boardIndex == index})
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
